d8ffd28a0a6d63f470f6b6ab16e3ef2c0ddab416
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2023-09-01 09:11:22 +0200

    Add train acc. to results csv

--- run_all.sh
+++ run_all.sh
@@ -5,8 +5,13 @@ root_dir="../cupbearer-experiments/"
 mkdir -p $root_dir
 cd "$root_dir"
 
+<<<<<<< HEAD
 #outputfile="$(realpath "results_$(date +%Y-%m-%d_%H-%M-%S).csv")"
 #echo "Model,Dataset,Backdoor,Detector,Train acc.,AUC_ROC,AP,Timestamp Model,Timestamp Detector" >> "${outputfile}"
+=======
+outputfile="$(realpath "results_$(date +%Y-%m-%d_%H-%M-%S).csv")"
+echo "Model,Dataset,Backdoor,Detector,Train acc.,AUC_ROC,AP" >> "${outputfile}"
+>>>>>>> af08d61 (Add train acc. to results csv)
 
 models=( "mlp" "cnn" )
 datasets=( "mnist" "cifar10" )
@@ -20,8 +25,12 @@ for model in "${models[@]}"; do
 
         for backdoor in "${backdoors[@]}"; do
 
+<<<<<<< HEAD
             ts_model="$(date +%Y-%m-%d_%H-%M-%S)"
             task_dir="${model}/${dataset}/${backdoor}/${ts_model}/"
+=======
+            task_dir="${model}/${dataset}/${backdoor}/$(date +%Y-%m-%d_%H-%M-%S)/"
+>>>>>>> af08d61 (Add train acc. to results csv)
             # TODO second might not be unique if run in parallel
             mkdir -p "$task_dir"
 
@@ -35,7 +44,11 @@ for model in "${models[@]}"; do
                 --train_data.backdoor.p_backdoor "0.1" \
                 --model "$model"
 
+<<<<<<< HEAD
             #train_acc=$(jq -r '.["10"]["train/accuracy"]' "${task_dir}/metrics.json")
+=======
+            train_acc=$(jq -r '.["10"]["train/accuracy"]' "${task_dir}/metrics.json")
+>>>>>>> af08d61 (Add train acc. to results csv)
 
             for detector in "${detectors[@]}"; do
 
@@ -64,11 +77,17 @@ for model in "${models[@]}"; do
                 #    --detector "from_run" \
                 #    --detector.path "$detector_dir"
 
+<<<<<<< HEAD
                 ##auc_roc=$(jq -r ".AUC_ROC" "${eval_dir}/eval.json")
                 ##ap=$(jq -r ".AP" "${eval_dir}/eval.json")
                 ##echo "${model},${dataset},${backdoor},${detector},${train_acc},${auc_roc},${ap},${ts_model},${ts_detector}" >> "${outputfile}"
 
                 break #TODO tmp
+=======
+                auc_roc=$(jq -r ".AUC_ROC" "${eval_dir}/eval.json")
+                ap=$(jq -r ".AP" "${eval_dir}/eval.json")
+                echo "${model},${dataset},${backdoor},${detector},${train_acc},${auc_roc},${ap}" >> "${outputfile}"
+>>>>>>> af08d61 (Add train acc. to results csv)
 
             done
             break #TODO tmp
