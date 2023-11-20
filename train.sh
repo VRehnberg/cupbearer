f0f11fec99eb6aaf208bf2672d765696051a44d2
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2023-11-20 15:32:51 +0100

    Fix validation data cl-args

--- train.sh
+++ train.sh
@@ -23,19 +23,30 @@ for lr in $(python -c "import numpy as np; print( *( 10 ** (-4 * np.random.rand(
         --train_data.original "$dataset" \
         --train_data.backdoor "$backdoor" \
         --train_data.backdoor.p_backdoor "0.1" \
-        --val_data "backdoor" \
+        --val_data.val "backdoor" \
+        --val_data.val.original "$dataset" \
+        --val_data.val.original.train False \
+        --val_data.val.backdoor "$backdoor" \
+        --val_data.val.backdoor.p_backdoor "0.1" \
+        --val_data.val.backdoor.p_noise "0.0" \
+        --val_data.clean "backdoor" \
+        --val_data.clean.original "$dataset" \
+        --val_data.clean.original.train False \
+        --val_data.clean.backdoor "$backdoor" \
+        --val_data.clean.backdoor.p_backdoor "0.0" \
+        --val_data.clean.backdoor.p_noise "0.0" \
         --val_data.backdoor "backdoor" \
-        --val_data.backdoor.backdoor $backdoor \
-        --val_data.backdoor.original $dataset \
-        --val_data.backdoor.backdoor.p_backdoor "0.1" \
-        --val_data.original "backdoor" \
-        --val_data.original.backdoor $backdoor \
-        --val_data.original.original $dataset \
-        --val_data.original.backdoor.p_backdoor "0.0" \
-        --val_data.fiftyfifty "backdoor" \
-        --val_data.fiftyfifty.backdoor $backdoor \
-        --val_data.fiftyfifty.original $dataset \
-        --val_data.fiftyfifty.backdoor.p_backdoor "0.5" \
+        --val_data.backdoor.original "$dataset" \
+        --val_data.backdoor.original.train False \
+        --val_data.backdoor.backdoor "$backdoor" \
+        --val_data.backdoor.backdoor.p_backdoor "1.0" \
+        --val_data.backdoor.backdoor.p_noise "0.0" \
+        --val_data.custom "backdoor" \
+        --val_data.custom.original "$dataset" \
+        --val_data.custom.original.train False \
+        --val_data.custom.backdoor "$backdoor" \
+        --val_data.custom.backdoor.p_backdoor "0.0" \
+        --val_data.custom.backdoor.p_noise "1.0" \
         --optim.learning_rate=$lr \
         --num_epochs=100 \
         --model "$model"
