72a7eb5b9ed80b91d1a5ef21aa2db48d223ce042
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2023-11-22 15:25:22 +0100

    Make train.sh more flexible

--- train.sh
+++ train.sh
@@ -5,10 +5,15 @@ root_dir="../cupbearer-experiments/"
 mkdir -p $root_dir
 cd "$root_dir"
 
-N=1
-model="mlp"
-dataset="mnist"
+N=10
+model="cnn"
+dataset="cifar10"
 backdoor="wanet"
+
+unset is_wanet
+if [ "$backdoor" = "wanet" ]; then
+    is_wanet="True"
+fi
 
 # Train classifier
 for lr in $(python -c "import numpy as np; print( *( 10 ** (-4 * np.random.rand($N) ) ) )"); do
@@ -22,32 +27,31 @@ for lr in $(python -c "import numpy as np; print( *( 10 ** (-4 * np.random.rand(
         --train_data "backdoor" \
         --train_data.original "$dataset" \
         --train_data.backdoor "$backdoor" \
-        --train_data.backdoor.p_backdoor "0.1" \
+        --train_data.backdoor.p_backdoor 0.1 \
         --val_data.val "backdoor" \
         --val_data.val.original "$dataset" \
         --val_data.val.original.train False \
         --val_data.val.backdoor "$backdoor" \
         --val_data.val.backdoor.p_backdoor "0.1" \
-        --val_data.val.backdoor.p_noise "0.0" \
+        ${is_wanet:+"--val_data.val.backdoor.p_noise=0.0"} \
         --val_data.clean "backdoor" \
         --val_data.clean.original "$dataset" \
         --val_data.clean.original.train False \
         --val_data.clean.backdoor "$backdoor" \
         --val_data.clean.backdoor.p_backdoor "0.0" \
-        --val_data.clean.backdoor.p_noise "0.0" \
+        ${is_wanet:+"--val_data.clean.backdoor.p_noise=0.0"} \
         --val_data.backdoor "backdoor" \
         --val_data.backdoor.original "$dataset" \
         --val_data.backdoor.original.train False \
         --val_data.backdoor.backdoor "$backdoor" \
-        --val_data.backdoor.backdoor.p_backdoor "1.0" \
-        --val_data.backdoor.backdoor.p_noise "0.0" \
+        ${is_wanet:+"--val_data.backdoor.backdoor.p_noise=0.0"} \
         --val_data.custom "backdoor" \
         --val_data.custom.original "$dataset" \
         --val_data.custom.original.train False \
         --val_data.custom.backdoor "$backdoor" \
         --val_data.custom.backdoor.p_backdoor "0.0" \
-        --val_data.custom.backdoor.p_noise "1.0" \
+        ${is_wanet:+"--val_data.custom.backdoor.p_noise=1.0"} \
         --optim.learning_rate=$lr \
-        --num_epochs=100 \
+        --num_epochs=50 \
         --model "$model"
 done
